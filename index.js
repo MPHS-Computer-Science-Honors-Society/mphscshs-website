2932aa52f25cb0e52b55ac3c5d7f991a3698bfa4
byrrron <maxbyronb@gmail.com>
2024-08-05 01:21:44 -0400

    8/4-8/5 session (removed text boxes, fixed topnav)

---
+++ index.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+    var y = document.getElementById("menuitems");
+    if (y.className === "flexer") {
+        y.className -= " flexer";
+    } else {
+        y.className = "flexer";
+    }
+}
---
+++ tilt.js
@@ -0,0 +1,27 @@
+let constrain = 5000;
+let mouseOverContainer = document.getElementById("ex1");
+let ex1Layer = document.getElementById("ex1-layer");
+
+function transforms(x, y, el) {
+let box = el.getBoundingClientRect();
+let calcX = -(y - box.y - (box.height / 2)) / constrain;
+let calcY = (x - box.x - (box.width / 2)) / constrain;
+
+return "perspective(100px) "
++ "   rotateX("+ calcX +"deg) "
++ "   rotateY("+ calcY +"deg) ";
+};
+
+function transformElement(el, xyEl) {
+el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function(e) {
+let xy = [e.clientX, e.clientY];
+let position = xy.concat([ex1Layer]);
+
+window.requestAnimationFrame(function(){
+transformElement(ex1Layer, position);
+});
+};
+
