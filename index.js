de0f3b27250934387b46f1c20d5fe54880b57e49
byrrron <maxbyronb@gmail.com>
2024-08-22 01:17:40 -0400

    leader page and footer and fixing bugs

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ function myFunction() {
         y.className = "flexer";
     }
 }
+function doTouch(event) {
+  var clientX = event.touches[0].clientX;
+  var clientY = event.touches[0].clientY;
+  pingEffect(clientX, clientY);
+}
+function doClick(event) {
+  // get the click co-ords
+  var clientX = event.clientX;
+  var clientY = event.clientY;
+  pingEffect(clientX, clientY);
+}
+
+var pingName = 'example';
+
+function pingEffect(clientX, clientY) {
+  // get and position the ping element
+  var ping = document.getElementById('ping');
+  ping.style.left = clientX+'px';
+  ping.style.top = clientY+'px';
+  ping.style.transform = 'translate(-50%, -50%)';
+  ping.style.display = 'block';
+  // run the animation
+  ping.classList.remove(pingName);
+  void ping.offsetWidth; // trigger a DOM reflow
+  ping.classList.add(pingName);
+}
+document.addEventListener('touchstart', doTouch);
+document.addEventListener('click', doClick);
--- tilt.js
+++ tilt.js
@@ -3,25 +3,30 @@ let mouseOverContainer = document.getElementById("ex1");
 let ex1Layer = document.getElementById("ex1-layer");
 
 function transforms(x, y, el) {
-let box = el.getBoundingClientRect();
-let calcX = -(y - box.y - (box.height / 2)) / constrain;
-let calcY = (x - box.x - (box.width / 2)) / constrain;
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - box.height / 2) / constrain;
+    let calcY = (x - box.x - box.width / 2) / constrain;
 
-return "perspective(100px) "
-+ "   rotateX("+ calcX +"deg) "
-+ "   rotateY("+ calcY +"deg) ";
-};
-
-function transformElement(el, xyEl) {
-el.style.transform  = transforms.apply(null, xyEl);
+    return (
+        "perspective(100px) " 
+        "   rotateX(" +
+        calcX +
+        "deg) " +
+        "   rotateY(" +
+        calcY +
+        "deg) "
+    );
 }
 
-mouseOverContainer.onmousemove = function(e) {
-let xy = [e.clientX, e.clientY];
-let position = xy.concat([ex1Layer]);
+function transformElement(el, xyEl) {
+    el.style.transform = transforms.apply(null, xyEl);
+}
 
-window.requestAnimationFrame(function(){
-transformElement(ex1Layer, position);
-});
+mouseOverContainer.onmousemove = function (e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([ex1Layer]);
+
+    window.requestAnimationFrame(function () {
+        transformElement(ex1Layer, position);
+    });
 };
-
