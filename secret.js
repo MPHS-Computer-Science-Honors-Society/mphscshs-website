4f62700c766f8480ef6b6e43ad6d918093bc6c2e
Shade Burgess <106717816+shadeburgess2025@users.noreply.github.com>
2024-09-05 22:06:28 -0400

    Add files via upload

---
+++ secret.js
@@ -0,0 +1,13 @@
+
+const button = document.getElementById("alecButton");
+let clickCount = 0;
+console.log(button);
+
+button.addEventListener("click", function() {
+    clickCount++;
+    console.log(clickCount);
+
+     if (clickCount === 5) {
+        button.href = "secret.html";
+    }
+});
