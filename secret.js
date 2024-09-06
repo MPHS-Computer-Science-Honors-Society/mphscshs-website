72504c3ae6aa0be0f5c9c73fe2c03d3daf22f5ce
Shade Burgess <106717816+shadeburgess2025@users.noreply.github.com>
2024-09-06 17:30:30 -0400

    Add files via upload

--- secret.js
+++ secret.js
@@ -1,12 +1,43 @@
 const button = document.getElementById("alecButton");
-let clickCount = 0;
-console.log(button);
+var clickCount = 0;
 
-button.addEventListener("click", function() {
+button.addEventListener('click', () => {
     clickCount++;
     console.log(clickCount);
 
-     if (clickCount === 5) {
+    if (clickCount === 15) {
         button.href = "secret.html";
     }
 });
+
+const exploder = document.getElementById("superSplode");
+var splodeCountdown = 15;
+
+exploder.addEventListener('click', () => {
+    splodeCountdown--;
+
+    if (splodeCountdown === 0)  {
+        exploder.src = "assets\\leaders\\jakob-dies.gif";
+    }
+});
+
+const shade = document.getElementById("shadeEffect");
+var shadeEffectCountdown = 8;
+var shadeEffectTrigger = false;
+
+shade.addEventListener('click', () => {
+    shadeEffectCountdown--;
+    console.log(shadeEffectCountdown);
+
+    if (shadeEffectCountdown === 0) {
+        console.log('gets here');
+        shadeEffectTrigger = true;
+    }
+});
+
+if (shadeEffectTrigger) {
+    console.log('gets here again');
+    let x = event.clientX;
+    let y = event.clientY;
+    shade.style.objectPosition = x + "px " + y + "px";
+}
