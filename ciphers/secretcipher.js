9af803d7e93c20ddc74bcc882c6a6ba0c769972c
Shade Burgess <shadeburgess2025@gmail.com>
2024-12-10 11:03:52 -0500

    fixed the secret cipher book

--- ciphers/secretcipher.js
+++ ciphers/secretcipher.js
@@ -32,7 +32,7 @@ secretSubmit.addEventListener('click', () => {
         }, 80);
 
         if (secretCount == 0) { 
-            secretBox.height = String(soHeight + secretResult.clientHeight + 75) + "px";
+            secretBox.style.height = String(soHeight + secretResult.clientHeight + 75) + "px";
             secretCount++;
         }
     }
@@ -56,12 +56,8 @@ function secretCipher(text, key, mode) {
     text = text.toUpperCase();
     key = key.toUpperCase();
     let resultText = '';
-    console.log(tempKey);
     let keyCount = 0;
-    console.log(text.length);
-    console.log(key);
     for (let i = 0; i < text.length; i++) {
-        console.log("key count: " + keyCount);
         const textChar = text[i];
         const keyChar = key[keyCount];
         if (alphabet.includes(textChar)) {
@@ -69,8 +65,6 @@ function secretCipher(text, key, mode) {
             const keyIndex = alphabet.indexOf(keyChar);
 
             let newIndex;
-            console.log("textChar: " + textChar + "\ntextIndex: " + textIndex);
-            console.log("keyChar: " + keyChar + "\nkeyIndex: " + keyIndex);
             newIndex = (Math.pow(textIndex, 2) + Math.pow(keyIndex, 2)) % 26;
             resultText += alphabet[newIndex];
             keyCount++;
